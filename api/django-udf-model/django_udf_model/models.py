24f04f82887f95a07e24342507835107c3fa1f6c
dameyerdave <dameyerdave@gmail.com>
2023-06-13 12:59:22 +0200

    [FEAT] Added Udf model

--- api/django-udf-model/django_udf_model/models.py
+++ api/django-udf-model/django_udf_model/models.py
@@ -1,20 +1,34 @@
+from django.db.models import Model
+from collections import OrderedDict
+from .query import TableFunctionManager
+
+
 class UdfConnectionRouter:
     def db_for_read(self, model, **hints):
-        if hasattr(model, 'using'):
+        if hasattr(model, "using"):
             return model.using
-        return 'default'
+        return "default"
 
     def db_for_write(self, model, **hints):
-        if hasattr(model, 'using'):
+        if hasattr(model, "using"):
             return model.using
-        return 'default'
+        return "default"
 
     def allow_relation(self, obj1, obj2, **hints):
-        if not hasattr(obj1, 'using') and not hasattr(obj2, 'using'):
+        if not hasattr(obj1, "using") and not hasattr(obj2, "using"):
             return True
         return False
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if db == 'default':
+        if db == "default":
             return True
         return False
+
+
+class UdfModel(Model):
+    function_args = OrderedDict()
+    objects = TableFunctionManager()
+
+    class Meta:
+        abstract = True
+        managed = False
