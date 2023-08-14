95398dffc1949a9508462578f736650d59f9e2a8
olloz26 <olloz@nexus.ethz.ch>
2023-08-14 11:27:38 +0200

    [FEAT] add requirements

--- api/django-common/setup.py
+++ api/django-common/setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup
 
-setup()
+setup(
+    install_requires=(
+        "python-magic>=0.4.0",
+        "hurry.filesize>=0.9.0",
+        "djangorestframework>=3.14.0",
+    )
+)
