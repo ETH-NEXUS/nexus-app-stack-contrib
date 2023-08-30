c5f43867a16b3792045dc005ca13def7c7e917a0
olloz26 <olloz@nexus.ethz.ch>
2023-08-30 08:54:06 +0200

    [FEAT] also checkout the top-level files

--- download.sh
+++ download.sh
@@ -8,7 +8,19 @@ git clone -n --depth=1 --filter=tree:0 https://"$TOKEN"@$(echo "$BRANCH" | sed -
 
 cd $nexus_app_stack_contrib_path
 
-git sparse-checkout set --no-cone "$@"
+case "$@" in
+  api/*)
+    cone_pattern_set="/api/* !/api/*/ $@"
+    ;;
+  cli/*)
+    cone_pattern_set="/cli/* !/cli/*/ $@"
+    ;;
+  ui/*)
+    cone_pattern_set="/ui/* !/ui/*/ $@"
+    ;;
+esac
+
+git sparse-checkout set "$cone_pattern_set"
 git checkout
 
 ls -d "$@" > /dev/null
