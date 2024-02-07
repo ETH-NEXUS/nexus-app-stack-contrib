91fb3f9e1d2aab6c3d6a2630a86056d706aac76a
olloz26 <olloz@nexus.ethz.ch>
2024-02-07 17:08:07 +0100

    [FEAT] add the "editable_fields" tool

--- api/django-common/django_common/admin.py
+++ api/django-common/django_common/admin.py
@@ -14,3 +14,22 @@ def create_model_admin(model_admin, name, model, verbose_name=None, verbose_name
     new_model = type(name, (model,), {"__module__": "", "Meta": Meta})
     admin.site.register(new_model, model_admin)
     return model_admin
+
+
+class EditableFieldsTabularInline(admin.TabularInline):
+    editable_fields = ()
+    exclude = ()
+
+    def get_readonly_fields(self, request, obj=None):
+        return (list(self.readonly_fields)
+                + [field.name for field in self.model._meta.fields
+                   if field.name not in self.editable_fields and field.name not in self.exclude])
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
