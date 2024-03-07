6fd5385070edcc0c79943cc251f580e072169518
olloz26 <olloz@nexus.ethz.ch>
2024-03-07 10:58:21 +0100

    [FEAT] add a field

--- api/django-common/django_common/serializers.py
+++ api/django-common/django_common/serializers.py
@@ -7,4 +7,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "first_name", "last_name", "email", "is_superuser", "groups")
+        fields = ("id", "username", "first_name", "last_name", "email", "is_staff", "is_superuser", "groups")
