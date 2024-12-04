3c466bf8d08b8080fe6aba1ca6da7fff232d3ed9
olloz26 <olloz@nexus.ethz.ch>
2024-12-04 17:04:30 +0100

    [FEAT] improve dependencies (next try)

--- api/django-fileupload/setup.py
+++ api/django-fileupload/setup.py
@@ -4,8 +4,7 @@ from setuptools import setup
 
 setup(
     install_requires=[
-        re.sub(r"^-e /([a-z-/]+)/([a-z-]+)$", r"\2 @ file:///\1/\2", x) if x.startswith("-e ")
-        else x
+        re.sub(r"^-e /([a-z-/]+)/([a-z-]+)$", r"\2@ file:///\1/\2", x) if x.startswith("-e ") else x
         for x in open("requirements.txt", "r").read().splitlines()
     ]
 )
