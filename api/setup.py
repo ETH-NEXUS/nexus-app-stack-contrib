87b0247e06995683b45cb757570a71503b13325d
olloz26 <olloz@nexus.ethz.ch>
2024-12-05 15:01:53 +0100

    [FEAT] improve dependencies (next try)

--- api/setup.py
+++ api/setup.py
@@ -1,10 +1,14 @@
+import os
 import re
 
 from setuptools import setup
 
-setup(
-    install_requires=[
-        re.sub(r"^-e /([a-z-/]+)/([a-z-]+)$", r"\2@ file:///\1/\2", x) if x.startswith("-e ") else x
-        for x in open("requirements.txt", "r").read().splitlines()
-    ]
-)
+if os.path.exists("requirements.txt"):
+    setup(
+        install_requires=[
+            re.sub(r"^-e /([a-z-/]+)/([a-z-]+)$", r"\2@ file:///\1/\2", x) if x.startswith("-e ") else x
+            for x in open("requirements.txt", "r").read().splitlines()
+        ]
+    )
+else:
+    setup()
