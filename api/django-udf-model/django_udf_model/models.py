4d579291d055806710d010a5681f7fe8de01697e
olloz26 <olloz@nexus.ethz.ch>
2025-01-09 18:16:46 +0100

    [FEAT] add proper UDF database exception handling

--- api/django-udf-model/django_udf_model/models.py
+++ api/django-udf-model/django_udf_model/models.py
@@ -1,5 +1,12 @@
-from django.db.models import Model
 from collections import OrderedDict
+
+from django import forms
+from django.contrib.admin import ModelAdmin
+from django.db import DatabaseError
+from django.db.models import Model
+from django.forms import formset_factory
+from django.forms.formsets import all_valid
+
 from .query import TableFunctionManager
 
 
@@ -33,6 +40,59 @@ class UdfModel(Model):
         abstract = True
         managed = False
         base_manager_name = "objects"
+
+
+class UdfModelAdmin(ModelAdmin):
+    _formsets = None
+
+    def _create_formsets(self, request, obj, change):
+        formsets, inline_instances = super()._create_formsets(request, obj, change)
+        self._formsets = formsets # Hack
+        return formsets, inline_instances
+
+    def save_form(self, request, form, change):
+        new_object = super().save_form(request, form, change)
+        try:
+            new_object.save()
+            if all_valid(self._formsets):
+                form.save_m2m()
+                for formset in self._formsets:
+                    self.save_formset(request, form, formset, change=change)
+        except DatabaseError as e:
+            # Hack: afterwards the all_valid(formsets) function is called in the ModelAdmin._changeform_view(...) method
+            # and fails. As a result, the exception message is passed to the user interface.
+            form.add_error(None, f"An error occurred: {str(e)}")
+            class Invalid(forms.Form):
+                pass
+            self._formsets.append(formset_factory(Invalid)())
+        return new_object
+
+    def save_model(self, request, obj, form, change):
+        # At this point it is too late to handle database exceptions.
+        pass
+
+    def save_related(self, request, form, formsets, change):
+        # At this point it is too late to handle database exceptions.
+        pass
+
+    # TODO Use or remove.
+    # def response_add(self, request, obj, post_url_continue=None):
+    #     from django.contrib import messages
+    #     m = messages.get_messages(request)
+    #     for message in m:
+    #         if message.level == messages.ERROR:
+    #             m.used = False
+    #             return self.response_post_save_add(request, obj)
+    #     return super().response_add(request=request, obj=obj, post_url_continue=post_url_continue)
+    #
+    # def response_change(self, request, obj):
+    #     from django.contrib import messages
+    #     m = messages.get_messages(request)
+    #     for message in m:
+    #         if message.level == messages.ERROR:
+    #             m.used = False
+    #             return self.response_post_save_change(request, obj)
+    #     return super().response_change(request=request, obj=obj)
 
 
 def model_with_udf_manager(model_class):
