19ae3d3f87058bb077fcebbcaeaf155bba04985e
olloz26 <olloz@nexus.ethz.ch>
2026-02-24 11:27:39 +0100

    [FEAT] add "TabularInline" whitelisting (next try)

--- api/django-common/django_common/admin.py
+++ api/django-common/django_common/admin.py
@@ -23,30 +23,55 @@ def create_model_admin(model_admin, name, model, verbose_name=None, verbose_name
     return model_admin
 
 
-def _extend_get_queryset_method(model_class, model, get_filter_queryset_function):
-    original_get_queryset = model_class.get_queryset
-    filter_queryset = get_filter_queryset_function(model)
-    if filter_queryset:
-        def new_get_queryset(self, request):
-            queryset = original_get_queryset(self, request)
-            queryset = filter_queryset(queryset, request)
-            return queryset
-
-        model_class.get_queryset = new_get_queryset
+def _extend_get_queryset_method(model_admin_class, filter_queryset):
+    original_get_queryset = model_admin_class.get_queryset
+    def new_get_queryset(self, request):
+        queryset = original_get_queryset(self, request)
+        queryset = filter_queryset(queryset, request)
+        return queryset
+    model_admin_class.get_queryset = new_get_queryset
 
 
-def whitelist(get_filter_queryset_function):
-    def decorator(model_class):
-        _extend_get_queryset_method(model_class, model_class.model, get_filter_queryset_function)
-        return model_class
+def _whitelist_inline(get_filter_queryset_function):
+    def decorator(model_admin_class):
+        filter_queryset_function = get_filter_queryset_function(model_admin_class.model)
+
+        if filter_queryset_function is not None:
+            _extend_get_queryset_method(model_admin_class, filter_queryset_function)
+
+            original_get_formset = model_admin_class.get_formset
+            def new_get_formset(self, request, obj=None, **kwargs):
+                formset = original_get_formset(self, request, obj, **kwargs)
+                assert not hasattr(formset, "_request")
+                formset._request = request
+                return formset
+            model_admin_class.get_formset = new_get_formset
+
+            original_add_fields = model_admin_class.formset.add_fields
+            def new_add_fields(self, form, index):
+                # The super method must be called first, as this is where the "id" field is added to the form.
+                result = original_add_fields(self, form, index)
+                if index is not None:
+                    id_field = form.fields["id"]
+                    id_field.queryset = filter_queryset_function(id_field.queryset, self._request)
+                return result
+            model_admin_class.formset.add_fields = new_add_fields
+
+        return model_admin_class
     return decorator
 
 
 def register_and_whitelist(model, get_filter_queryset_function):
-    def decorator(model_class):
-        admin.register(model)(model_class)
-        _extend_get_queryset_method(model_class, model, get_filter_queryset_function)
-        return model_class
+    def decorator(model_admin_class):
+        admin.register(model)(model_admin_class)
+        filter_queryset_function = get_filter_queryset_function(model)
+
+        if filter_queryset_function is not None:
+            _extend_get_queryset_method(model_admin_class, filter_queryset_function)
+
+        for inline in model_admin_class.inlines:
+            _whitelist_inline(get_filter_queryset_function)(inline)
+        return model_admin_class
     return decorator
 
 
